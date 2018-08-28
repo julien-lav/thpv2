f2db93206ae3287511cd7a3bccac5b586f694ded
julien-lav <julien.laville@hotmail.fr>
2018-08-28 12:05:06 +0200

    Refactoe ! ;D

--- tictactoe.rb
+++ tictactoe.rb
@@ -39,28 +39,16 @@ class Game
    	def winning_combos
    		board_winner = false
 
-		if ( # I have to refactor this but for now that's working fine
-            (
-             (@arr[0] == "X" && @arr[1] == "X" && @arr[2] == "X") ||
-       		   (@arr[3] == "X" && @arr[4] == "X" && @arr[5] == "X") ||
-       		   (@arr[6] == "X" && @arr[7] == "X" && @arr[8] == "X") ||
-       		   (@arr[0] == "X" && @arr[3] == "X" && @arr[6] == "X") ||
-       		   (@arr[1] == "X" && @arr[4] == "X" && @arr[7] == "X") ||
-       		   (@arr[2] == "X" && @arr[5] == "X" && @arr[8] == "X") ||
-       		   (@arr[0] == "X" && @arr[4] == "X" && @arr[8] == "X") ||
-       		   (@arr[2] == "X" && @arr[4] == "X" && @arr[6] == "X")
-       	    ) ||
-       	   (
-            (@arr[0] == "O" && @arr[1] == "O" && @arr[2] == "O") ||
-       		  (@arr[3] == "O" && @arr[4] == "O" && @arr[5] == "O") ||
-       		  (@arr[6] == "O" && @arr[7] == "O" && @arr[8] == "O") ||
-       		  (@arr[0] == "O" && @arr[3] == "O" && @arr[6] == "O") ||
-       		  (@arr[1] == "O" && @arr[4] == "O" && @arr[7] == "O") ||
-       		  (@arr[2] == "O" && @arr[5] == "O" && @arr[8] == "O") ||
-       		  (@arr[0] == "O" && @arr[4] == "O" && @arr[8] == "O") ||
-       		  (@arr[2] == "O" && @arr[4] == "O" && @arr[6] == "O")
-       	   )
-          )
+		 if (           
+             ((@arr[0] == @arr[1] && @arr[1] == @arr[2]) && (@arr[0] == "X" || @arr[0] == "O")) ||
+             ((@arr[3] == @arr[4] && @arr[4] == @arr[5]) && (@arr[3] == "X" || @arr[3] == "O")) ||
+             ((@arr[6] == @arr[7] && @arr[7] == @arr[8]) && (@arr[6] == "X" || @arr[6] == "O")) ||
+             ((@arr[0] == @arr[3] && @arr[3] == @arr[6]) && (@arr[0] == "X" || @arr[0] == "O")) ||
+             ((@arr[1] == @arr[4] && @arr[4] == @arr[7]) && (@arr[1] == "X" || @arr[1] == "O")) ||
+             ((@arr[2] == @arr[5] && @arr[5] == @arr[8]) && (@arr[2] == "X" || @arr[2] == "O")) ||
+             ((@arr[0] == @arr[4] && @arr[4] == @arr[8]) && (@arr[0] == "X" || @arr[0] == "O")) ||
+             ((@arr[2] == @arr[4] && @arr[4] == @arr[6]) && (@arr[2] == "X" || @arr[2] == "O"))                 
+               )
 		     board_winner = true
 			if board_winner == true
 					puts "#{@current_player} just win !"
